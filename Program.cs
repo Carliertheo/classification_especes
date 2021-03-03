d791c96d791b03b00fefd65216814388ca739fdf
Theo Carlier <theo@MacBook-Pro-de-Theo.local>
2021-03-03 09:54:26 +0100

    nouvel modif 03/02/21

--- Program.cs
+++ Program.cs
@@ -7,50 +7,54 @@ namespace classification_especes
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //ceci est un test 
         }
 
     }
 
-    class monde_vivant
+    class Monde_vivant
     {
-                protected static int niveauClassification = 1;
+        protected string nom_Monde_vivant;
     }
 
-    class domaine : monde_vivant
+    class Domaine : Monde_vivant
     {
-
+        protected string nom_Domaine;
+        public string Nom_Domaine
+        {
+            get { return nom_Domaine; }
+            set { nom_Domaine = Value; }
+        }
     }
 
-    class regne : domaine 
+    class Regne : Domaine 
     {
-
+        protected string nom_Regne;
     }
 
-    class embranchement : regne 
+    class Embranchement : Regne 
     {
-
+        protected string nom_Embranchement;
     }
 
-    class classe : ordre 
+    class Classe : Ordre 
     {
-
+        protected string nom_Classe;
     }
-    class ordre : famille
+    class Ordre : Famille
     {
-
+        protected string nom_Ordre;
     }
-    class famille : genre 
+    class Famille : Genre 
     {
-
+        protected string nom_Famille;
     }
-    class genre : espece
+    class Genre : Espece
     {
-
+        protected string nom_Genre;
     }
-    class espece
+    class Espece
     {
-
+        protected string nom_Espece;
     }
 
 }
