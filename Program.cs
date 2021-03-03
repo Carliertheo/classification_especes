9504128246eccf4460a19730aababc150d4193c2
Theo Carlier <theo@MacBook-Pro-de-Theo.local>
2021-03-03 09:30:46 +0100

    nouvel modif

--- Program.cs
+++ Program.cs
@@ -9,5 +9,48 @@ namespace classification_especes
             Console.WriteLine("Hello World!");
             //ceci est un test 
         }
+
     }
+
+    class monde_vivant
+    {
+                protected static int niveauClassification = 1;
+    }
+
+    class domaine : monde_vivant
+    {
+
+    }
+
+    class regne : domaine 
+    {
+
+    }
+
+    class embranchement : regne 
+    {
+
+    }
+
+    class classe : ordre 
+    {
+
+    }
+    class ordre : famille
+    {
+
+    }
+    class famille : genre 
+    {
+
+    }
+    class genre : espece
+    {
+
+    }
+    class espece
+    {
+
+    }
+
 }
